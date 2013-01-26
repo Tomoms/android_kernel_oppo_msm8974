6d9bad09d1ede16db7b0f853d52187fae022cc55
Eric W. Biederman <ebiederm@xmission.com>
2013-01-25 16:32:10 -0800

    userns: Allow the userns root to mount tmpfs.
    There is no backing store to tmpfs and file creation rules are the
    same as for any other filesystem so it is semantically safe to allow
    unprivileged users to mount it.  ramfs is safe for the same reasons so
    allow either flavor of tmpfs to be mounted by a user namespace root
    user.
    
    The memory control group successfully limits how much memory tmpfs can
    consume on any system that cares about a user namespace root using
    tmpfs to exhaust memory the memory control group can be deployed.
    
    Change-Id: Ie237fefe71aaa39172d661945197a20e6d0280eb
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>

--- mm/shmem.c
+++ mm/shmem.c
@@ -2879,6 +2879,7 @@ static struct file_system_type shmem_fs_type = {
 	.name		= "tmpfs",
 	.mount		= shmem_mount,
 	.kill_sb	= kill_litter_super,
+	.fs_flags	= FS_USERNS_MOUNT,
 };
 
 int __init shmem_init(void)
@@ -2936,6 +2937,7 @@ static struct file_system_type shmem_fs_type = {
 	.name		= "tmpfs",
 	.mount		= ramfs_mount,
 	.kill_sb	= kill_litter_super,
+	.fs_flags	= FS_USERNS_MOUNT,
 };
 
 int __init shmem_init(void)
