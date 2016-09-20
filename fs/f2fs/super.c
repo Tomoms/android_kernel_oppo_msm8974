4359fb11609ee9defbb2bcb86f90c7ce58cf7b79
Jaegeuk Kim <jaegeuk@kernel.org>
2016-09-19 17:55:10 -0700

    f2fs: use crc and cp version to determine roll-forward recovery
    Previously, we used cp_version only to detect recoverable dnodes.
    In order to avoid same garbage cp_version, we needed to truncate the next
    dnode during checkpoint, resulting in additional discard or data write.
    If we can distinguish this by using crc in addition to cp_version, we can
    remove this overhead.
    
    There is backward compatibility concern where it changes node_footer layout.
    So, this patch introduces a new checkpoint flag, CP_CRC_RECOVERY_FLAG, to
    detect new layout. New layout will be activated only when this flag is set.
    
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    
     Conflicts:
    	fs/f2fs/recovery.c
    
    Change-Id: I414e3c69d5800dc53f699a868b4dd71c0195a6ab

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -1851,6 +1851,9 @@ try_onemore:
 		if (need_fsck)
 			set_sbi_flag(sbi, SBI_NEED_FSCK);
 
+		if (!retry)
+			goto skip_recovery;
+
 		err = recover_fsync_data(sbi, false);
 		if (err < 0) {
 			need_fsck = true;
@@ -1868,7 +1871,7 @@ try_onemore:
 			goto free_kobj;
 		}
 	}
-
+skip_recovery:
 	/* recover_fsync_data() cleared this already */
 	clear_sbi_flag(sbi, SBI_POR_DOING);
 
