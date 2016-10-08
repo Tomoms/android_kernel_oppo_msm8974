3ba68685a60e814255dfb33e2aea8f00b722ff0f
John Stultz <john.stultz@linaro.org>
2016-10-07 17:02:29 -0700

    proc: add LSM hook checks to /proc/<tid>/timerslack_ns
    As requested, this patch checks the existing LSM hooks
    task_getscheduler/task_setscheduler when reading or modifying the task's
    timerslack value.
    
    Previous versions added new get/settimerslack LSM hooks, but since they
    checked the same PROCESS__SET/GETSCHED values as existing hooks, it was
    suggested we just use the existing ones.
    
    Link: http://lkml.kernel.org/r/1469132667-17377-2-git-send-email-john.stultz@linaro.org
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: "Serge E. Hallyn" <serge@hallyn.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Arjan van de Ven <arjan@linux.intel.com>
    Cc: Oren Laadan <orenl@cellrox.com>
    Cc: Ruchi Kandoi <kandoiruchi@google.com>
    Cc: Rom Lemarchand <romlem@android.com>
    Cc: Todd Kjos <tkjos@google.com>
    Cc: Colin Cross <ccross@android.com>
    Cc: Nick Kralevich <nnk@google.com>
    Cc: Dmitry Shmidt <dimitrysh@google.com>
    Cc: Elliott Hughes <enh@google.com>
    Cc: James Morris <jmorris@namei.org>
    Cc: Android Kernel Team <kernel-team@android.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Change-Id: Iaefe19cc837589e83c4897a859d0ced5fc9836e0

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -2404,6 +2404,12 @@ static ssize_t timerslack_ns_write(struct file *file, const char __user *buf,
 		}
 	}
 
+	err = security_task_setscheduler(p);
+	if (err) {
+		count = err;
+		goto out;
+	}
+
 	task_lock(p);
 	if (slack_ns == 0)
 		p->timer_slack_ns = p->default_timer_slack_ns;
@@ -2437,6 +2443,10 @@ static int timerslack_ns_show(struct seq_file *m, void *v)
 		if (err)
 			goto out;
 	}
+
+	err = security_task_getscheduler(p);
+	if (err)
+		goto out;
 
 	task_lock(p);
 	seq_printf(m, "%llu\n", p->timer_slack_ns);
