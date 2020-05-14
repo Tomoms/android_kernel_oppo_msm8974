8e3e06a3030d8739436ed3e8b28634cadd3701cc
Tomoms <fondatommaso2@gmail.com>
2020-05-14 19:22:40 +0200

    Nuke the old shell scripts

--- clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export ARCH=arm
-export SUBARCH=arm
-export CROSS_COMPILE=/run/media/tfonda/HDD/gcc-arm-9.2-2019.12-x86_64-arm-none-linux-gnueabihf/bin/arm-none-linux-gnueabihf-
-
-make mrproper
--- zip.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cp arch/arm/boot/zImage ../AnyKernel3/
-cd ../AnyKernel3
-zip -r ../$1 anykernel.sh dt META-INF/ tools/ zImage
